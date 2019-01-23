51b89344d355e870994c1fa1e68211758df29eb0
lakshyabatman <lakshya.khera@gmail.com>
2019-01-23 14:59:45 +0530

    Api started

---
+++ src/server.js
@@ -0,0 +1,41 @@
+const express =require('express');
+const hbs=require('hbs');
+const path=require('path');
+
+
+const app= express();
+app.set("views",path.join(__dirname+"/views"));
+hbs.registerPartials(__dirname + '/views/partial')
+app.set('view engine' ,'hbs');
+
+app.use((res,req,next)=>{
+    console.log("Hi");
+    next();
+})
+
+app.use(express.static(__dirname + "/public"));
+
+
+hbs.registerHelper("date",()=>{
+    return new Date().getFullYear()
+})
+
+app.get("/",(req,res)=>{
+    res.render("home.hbs",{
+        "title":"Home Page",
+
+    })
+  
+})
+
+
+app.get("/about",(req,res)=>{
+    res.render("about.hbs",{
+        "title":"About Page",
+    })
+})
+
+
+app.listen(4000,function(){
+    console.log("Server started at Port 4000")
+})
